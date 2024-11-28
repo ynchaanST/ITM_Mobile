be112434d425cf74e1f2e0b671a80dd02b7b574d
Kyle Yang <116270725+ynchaanST@users.noreply.github.com>
2024-11-29 03:32:21 +0900

    booking rebuild

--- app/src/main/java/com/example/mychelin_page/ReservationFragment.kt
+++ app/src/main/java/com/example/mychelin_page/ReservationFragment.kt
@@ -5,10 +5,41 @@ import android.os.Bundle
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import android.widget.Button
+import android.widget.Toast
+import androidx.navigation.fragment.findNavController
 import com.example.mychelin_page.R
 
 class ReservationFragment : Fragment(R.layout.fragment_reservation) {
-    override fun onCreateView(inflater: LayoutInflater, container: ViewGroup?, savedInstanceState: Bundle?): View? {
-        return inflater.inflate(R.layout.fragment_reservation, container, false)
+
+    private lateinit var btnBooking: Button
+
+    override fun onCreateView(
+        inflater: LayoutInflater,
+        container: ViewGroup?,
+        savedInstanceState: Bundle?
+    ): View? {
+        val view = inflater.inflate(R.layout.fragment_reservation, container, false)
+
+        initializeViews(view)
+        setupListeners()
+
+        return view
+    }
+
+
+    private fun initializeViews(view: View) {
+        btnBooking = view.findViewById(R.id.resConfirmButton)
+    }
+
+    private fun setupListeners() {
+        btnBooking.setOnClickListener {
+            try {
+                findNavController().navigate(R.id.page_booking)
+            } catch (e: Exception) {
+                Toast.makeText(context, "Failed to open Notice: ${e.message}", Toast.LENGTH_SHORT)
+                    .show()
+            }
+        }
     }
 }
